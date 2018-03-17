db9646bf0f5d355caebbacdc56895c88c6a53ce3
Dmitriy Chvanov <chapaev611@mail.ru>
2018-03-17 20:10:12 +0300

    started test

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,32 @@
 $(function() {
 
-	// Custom JS
+	function test() {
+		console.log('The function');
+	}
+
+	var $init = []; // массив вызываемых функций
+
+	window.onload = function() {
+		for (var i in $init) {
+			if (typeof($init) == 'function') {
+				init[i](); // вызов всех функций
+			}
+		}
+	}
+
+	$init.push(test());
+
+
+	// Второе решение
+	function ready() {
+		setTimeout(function () {
+			var js = document.createElement('script');
+			js.src = 'js/file.js';
+			js.async = true;
+			js.defer = true;
+		}, 10);
+	}
+
+	document.addEventListener('DOMContentLoaded', ready);
 
 });
